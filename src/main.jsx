626f040287bbab9ba65b9246acdc280d2bced1f6
pranav <prasharpranav@gmail.com>
2026-06-24 13:05:17 +0530

    Integrate Sentry for error monitoring and performance tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as Sentry from "@sentry/react"
+
+// Initialize Sentry
+Sentry.init({
+  dsn: "https://beb339a7ed1ccd2a268d494d8a97ff92@c451161921191968.ingest.de.sentry.io/4511619223978864",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/tfcproducts\.tfcmockup\.com/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
