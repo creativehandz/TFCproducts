e85a657de556306c4d463dad570d12e57896a930
pranav <prasharpranav@gmail.com>
2026-07-01 14:06:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -382,7 +382,7 @@ function App() {
                 <div className="col">
                 <div className="service-card" style={{ background: '#201F1F' }}>
                   <h3>
-                    Real Estate <br></br>Portal
+                    Real Estate <br></br>Portal.
                     <svg className="card-title-arrow" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M12 5V19M12 19L5 12M12 19L19 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
